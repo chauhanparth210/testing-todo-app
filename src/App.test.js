05cbb9e9d364ec1860e4e2780a46d92d2a688d10
parth.chauhan <parth.chauhan@innovaccer.com>
2022-03-10 11:37:31 +0530

    chore(testing-setup): setup react testing with jest and enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App component render", () => {
+  it("should contain text `learn react`", () => {
+    const component = shallow(<App debug />);
+    const linkElementText = component.find(".App-link").text();
+    expect(linkElementText).toMatch(/learn react/i);
+  });
 });
