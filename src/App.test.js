ff9e0b5a9ddcbdc16471779fe389fd6549d50d70
parth.chauhan <parth.chauhan@innovaccer.com>
2022-03-10 13:20:30 +0530

    chore: remove unused code

--- src/App.test.js
+++ src/App.test.js
@@ -2,10 +2,10 @@ import React from "react";
 import { shallow } from "enzyme";
 import App from "./App";
 
-describe("App component render", () => {
-  it("should contain text `learn react`", () => {
-    const component = shallow(<App debug />);
-    const linkElementText = component.find(".App-link").text();
-    expect(linkElementText).toMatch(/learn react/i);
-  });
-});
+// describe("App component render", () => {
+//   it("should contain text `learn react`", () => {
+//     const component = shallow(<App debug />);
+//     const linkElementText = component.find(".App-link").text();
+//     expect(linkElementText).toMatch(/learn react/i);
+//   });
+// });
